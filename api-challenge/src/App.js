0d8a20fd806a3c9766adc1ef7ca21d8c1013787e
Nhoj <nhojplummer@yahoo.com>
2023-06-01 15:18:35 -0400

    Setup for challenge done

--- api-challenge/src/App.js
+++ api-challenge/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { API } from './components';
 
 function App() {
   return (
     <div className="App">
-     
+     <API />
     </div>
   );
 }
---
+++ api-challenge/src/components/apitest.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function apitest() {
+  return <div>apitest</div>
+}
+
+export default apitest
